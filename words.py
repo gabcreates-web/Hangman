70e4a0f570ae7f259c9645eb365bd3724119a926
gabcreates-web <gabrielisthebest696969@gmail.com>
2026-06-08 20:59:25 -0500

    Create words.py

---
+++ words.py
@@ -0,0 +1,14 @@
+word_list = {
+    "Animals": ["dog", "lion", "tiger", "eagle", "elephant", "giraffe", "dolphin", "penguin", "cheetah", "kangaroo"],
+
+    "Foods": ["cake", "pizza", "bacon", "eggplant", "burger", "sushi", "pasta", "taco", "donut", "sandwich"],
+
+    "Countries": ["canada", "brazil", "japan", "germany", "mexico", "india", "france", "australia", "egypt", "italy"],
+
+    "Sports": ["soccer", "basketball", "tennis", "cricket", "baseball", "volleyball", "hockey", "rugby", "golf", "boxing"],
+
+    "Fruits": ["apple", "banana", "orange", "grape", "strawberry", "mango", "pineapple", "watermelon", "peach", "blueberry"],
+
+    "Programming": ["python", "java", "javascript", "variable", "function", "loop", "string", "compiler", "syntax", "debug"]
+}
+//feel free to add some more words of your own.
